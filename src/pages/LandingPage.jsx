3a935f1396094f5ca2fed0e56796c0a1b79b94e6
Sumeet Shelar <sumeetshelar2000@gmail.com>
2024-07-27 02:26:45 +0530

    Leaderboard, features section, Fancybtn

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import Faq from '../components/Faq'
+import Leaderboard from '../components/Leaderboard'
+import Features from '../components/Features'
 
 const LandingPage = () => {
   return (
     <div className='bg-[#16237f] size-full'>
       <Hero />
       <About />
+      <Leaderboard />
+      <Features />
       <Faq />
     </div>
   )
