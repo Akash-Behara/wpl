88628d9986ae75d191a8a91b8e9646ca6c4e4b68
Sumeet Shelar <sumeetshelar2000@gmail.com>
2024-08-02 16:20:41 +0530

    Updated marquee card back section

--- src/components/MarqueeSection.jsx
+++ src/components/MarqueeSection.jsx
@@ -3,17 +3,83 @@ import React from 'react'
 import Marquee from "./ui/marquee";
 
 import marqueeCard from '../assets/subtract/marquee_card.svg'
-import marqueeWolf from '../assets/svg/marquee_wolf.svg'
+
 import FancyButton from './FancyButton';
 
 import startEarnPng from '../assets/subtract_png/start_earn.png'
 import startEarnHoverPng from '../assets/subtract_png/start_earn_hover.png'
 
 const data = [
-    {text: '#1', occ: "REDWOLF"},
-    {text: '#2', occ: "ICEWOLF"},
-    {text: '#3', occ: "THUNDERWOLF"},
-    {text: '#4', occ: "BLACKWOLF"},
+    {
+        text: '1', 
+        occ: "BLACKWOLF", 
+        points:2500
+    },
+    {
+        text: '2', 
+        occ: "REDWOLF", 
+        points:2500
+    },
+    {
+        text: '3', 
+        occ: "ARTICWOLF", 
+        points:2500
+    },
+    {
+        text: '4', 
+        occ: "GRAYWOLF", 
+        points:2500
+    },
+    {
+        text: '5', 
+        occ: "CELESTIALWOLF", 
+        points:2500
+    },
+    {
+        text: '6', 
+        occ: "FIREWOLF", 
+        points:2500
+    },
+    {
+        text: '7', 
+        occ: "PURPLEWOLF", 
+        points:2500
+    },
+    {
+        text: '8', 
+        occ: "ICEWOLF", 
+        points:2500
+    },
+    {
+        text: '9', 
+        occ: "THUNDERWOLF", 
+        points:2500
+    },
+    {
+        text: '10', 
+        occ: "WARG", 
+        points:2500
+    },
+    {
+        text: '11', 
+        occ: "WEREWOLF", 
+        points:2500
+    },
+    {
+        text: '12', 
+        occ: "FENRIR", 
+        points:2500
+    },
+    {
+        text: '13', 
+        occ: "DIREWOLF", 
+        points:2500
+    },
+    {
+        text: '14', 
+        occ: "WHITEWOLF", 
+        points:2500
+    },
 ]
 
 const WolfCard = ({key, wolf}) => {
@@ -24,15 +90,28 @@ const WolfCard = ({key, wolf}) => {
                     <img src={marqueeCard} alt='about card 1' className='size-full'/>
                     <div className='w-[80%] absolute bottom-4 left-1/2 -translate-x-1/2 '>
                         <div className='bg-[#16237f]/40 size-full absolute'/>
-                        <img src={marqueeWolf} alt='about card 1 img' className=''/>
+                        <img src={`src/assets/images/wolf_pack/wolf_${wolf.text}.png`} alt='about card 1 img' className=''/>    
                     </div>
                     <div className='absolute top-8 w-full text-start ps-7 pr-3'>
-                        <p className='text-[#E38070]'>{wolf.text}</p>
+                        <p className='text-[#E38070]'>#{wolf.text}</p>
                         <p className='text-[25px] text-primary font-bienvenue'>{wolf.occ}</p>
                     </div>
                 </div>
                 <div className='back'>
-                    BACK
+                    <img src={marqueeCard} alt='about card 1'/>
+                    <div className='absolute top-8 w-full text-start ps-7 pr-3'>
+                        <p className='text-[25px] text-primary font-bienvenue'>{wolf.occ}</p>
+                        <div className='border border-b-white opacity-30 my-2'></div>
+                        <div className='mb-2'>
+                            <p className='text-[#E38070]'>RANK</p>
+                            <p className=''>#{wolf.text}</p>
+                        </div>
+                        <div>
+                            <p className='text-[#E38070]'>XP REQUIRED</p>
+                            <p className=''>{wolf.points} WP</p>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
